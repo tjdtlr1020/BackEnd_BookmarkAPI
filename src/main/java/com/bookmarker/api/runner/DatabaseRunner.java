bdb2e1627e8ffbe11fb6fd7e034e0e34eb3d3f7e
qkrtjdtlr102 <qkrtjdtlr102@gmail.com>
2024-03-28 11:41:11 +0900

    DB runner update

--- src/main/java/com/bookmarker/api/runner/DatabaseRunner.java
+++ src/main/java/com/bookmarker/api/runner/DatabaseRunner.java
@@ -24,6 +24,7 @@ public class DatabaseRunner implements ApplicationRunner {
         try (Connection connection = dataSource.getConnection()) {
             log.info("DB URL = {}", connection.getMetaData().getURL());
             log.info("DB Username = {}", connection.getMetaData().getUserName());
+            log.info(">>>>>>>DB Product Vendor  = {}",connection.getMetaData().getDatabaseProductName());
         }
     }
 }
